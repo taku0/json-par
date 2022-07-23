2c13ad1bc3c2a62141d3312501d2c2012555972b
taku0 <mxxouy6x3m_github@tatapa.org>
2022-07-23 17:29:22 +0900

    Bump version to 3.0.0

--- json-par.el
+++ json-par.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2021 taku0
 ;;
 ;; Author: taku0 (http://github.com/taku0)
-;; Version: 2.1.0
+;; Version: 3.0.0
 ;; Package-Requires: ((emacs "24.4") (json-mode "1.7.0"))
 ;; Keywords: abbrev, convenience, files
 ;; URL: https://github.com/taku0/json-par
