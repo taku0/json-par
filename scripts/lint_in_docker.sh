f35f553fb146c40875540c873eaa3ff4dcf209c1
taku0 <mxxouy6x3m_github@tatapa.org>
2025-04-12 15:45:01 +0900

    Update CI

--- scripts/lint_in_docker.sh
+++ scripts/lint_in_docker.sh
@@ -5,7 +5,7 @@
 # WORKAROUND: disable for 24 to mitigate "Argument `_length' should appear
 # (as _LENGTH) in the doc string".
 # Indentation rules changed since 29.
-for version in 29 # 28 27 26 25 # 24
+for version in 30 29 # 28 27 26 25 # 24
 do
     docker \
         run \
